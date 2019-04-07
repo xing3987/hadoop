35d042fc08762d8ba9e6b5b98624e06f42b6e589
chenxing <xing3987@163.com>
2019-04-08 02:15:16 +0800

    hive

---
+++ hive/src/main/java/hive/MyJsonParser.java
@@ -0,0 +1,16 @@
+package hive;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * hive中如何定义自己的函数：
+ 1、先写一个java类（extends UDF,重载方法public C evaluate(A a,B b)），实现你所想要的函数的功能（传入一个json字符串和一个脚标，返回一个值）
+ 2、将java程序打成jar包，上传到hive所在的机器
+ 3、在hive命令行中将jar包添加到classpath ：
+ hive>add jar /root/hivetest/myjson.jar;
+ 4、在hive命令中用命令创建一个函数叫做myjson，关联你所写的这个java类
+ hive> create temporary function myjson as 'hive.MyJsonParser';
+
+ */
+public class MyJsonParser extends UDF {
+}
