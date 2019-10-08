cd79ab5bd6792f364aaeeb23c2e0d5dcb7439fdd
chenxing <xing3987@163.com>
2019-10-09 01:14:05 +0800

    add sample

--- 案例文档/案例一：app数据采集elk清洗入库（eggplant）/预处理启动脚本.bash
+++ 案例文档/案例一：app数据采集elk清洗入库（eggplant）/预处理启动脚本.bash
@@ -7,3 +7,5 @@ outpath=/app-log-data/clean/$day_str
 echo "准备清洗$day_str 的数据......"
 
 /root/apps/hadoop-2.8.1/bin/hadoop jar /root/data-clean.jar cn.edu360.app.log.mr.AppLogDataClean $inpath $outpath
+
+# if [[ $? eq 1 ]] ...send email(提前安装邮件服务器参照邮件文档)
